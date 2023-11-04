e20c2f04fe2d7011c7773c9957a5a99bfd1559bd
Theofanis Triantafillis <fanis_30fillis@outlook.com>
2023-11-04 17:41:56 +0200

    fixed knight movement

--- Chess/src/chess/Piece.java
+++ Chess/src/chess/Piece.java
@@ -258,6 +258,7 @@ class Knight extends Piece {
 		if(!moveIsLegal(newLoc)) {
 			throw new InvalidMoveException("Invalid move");
 		}
+		board.movePiece(loc, newLoc);
 	}
 
 	boolean moveIsLegal(Location newLoc) {
@@ -269,8 +270,13 @@ class Knight extends Piece {
 			return false;
 		}
 
-		if(Math.abs(newLoc.getRow()-loc.getRow()) == 1 &&
-				Math.abs(newLoc.getCol() - loc.getCol()) == 2 ) {
+		// gets the horizontal and vertical distance to make the if
+		// condition more readable
+		final int verticalDistance = Math.abs(newLoc.getRow() - loc.getRow());
+		final int horizontalDistance = Math.abs(newLoc.getCol() - loc.getCol());
+
+		if((verticalDistance == 1 && horizontalDistance == 2) ||
+				(verticalDistance == 2 && horizontalDistance == 1)) {
 			// short circuit logic makes sure that if the object doesn't exist
 			// then it won't check it's color therefore won't throw a 
 			// Not found exception 
@@ -283,7 +289,6 @@ class Knight extends Piece {
 				return false;
 			}
 		}
-
 		return true;
 	}
 }
