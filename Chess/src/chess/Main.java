5b24d7d3d8cca76a430b6780679094ffb377bf6f
Theofanis Triantafillis <fanis_30fillis@outlook.com>
2023-11-04 19:11:50 +0200

    removed useless comments

--- Chess/src/chess/Main.java
+++ Chess/src/chess/Main.java
@@ -2,16 +2,6 @@ package chess;
 
 public class Main {
 	public static void main(String[] args) {
-		// Board b = new Board();
-//		b.init();
-//		System.out.println(b.toString());
-//		System.out.println(b.board[0][6].rep);
-//		try {
-//			b.board[0][6].moveTo(new Location(2,5));
-//		} catch (Exception e){
-//			System.out.println(e);
-//		}
-//		System.out.println(b.toString());
 		Game game = new Game();
 		game.play();
 	}
