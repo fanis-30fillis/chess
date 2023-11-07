c42e69985706e85d3e780bc804b8e05679676266
Theofanis Triantafillis <fanis_30fillis@outlook.com>
2023-11-07 09:48:33 +0200

    Added comments

--- Chess/src/chess/Pawn.java
+++ Chess/src/chess/Pawn.java
@@ -1,30 +1,41 @@
 package chess;
 
 class Pawn extends Piece {
-	boolean hasMoved;
+
+	boolean hasMoved = false;
+
 	Pawn (Location loc, Color c, Board b) {
+		// calls the super constructor so that we don't repeat code
 		super(loc, c, b);
+		// sets the representation 
 		rep = c == Color.WHITE ? "P" : "p";
 	}
 
 	void moveTo(Location newLoc) throws InvalidMoveException {
+		// pawn moveTo is the same as the Piece's 
 		super.moveTo(newLoc);
+		// sets the hasMoved which changes the pawns possible movements
 		hasMoved = true;
 	}
 
 	boolean moveIsLegal(Location newLoc) 
 	{
 
+		// performs the standard location checks
 		if(!standardLocationChecks(newLoc)) {
 			return false;
 		}
 
 		// we can't move backwards or sideways
+		// if the pawn is WHITE
 		if(this.color == Color.WHITE) {
+			// we can't move backwards
 			if(newLoc.getRow() <= loc.getRow()) {
 				return false;
 			}
 		} else {
+			// the pawn is BLACK 
+			// we can't move backwards
 			if(newLoc.getRow() >= loc.getRow()) {
 				return false;
 			}
